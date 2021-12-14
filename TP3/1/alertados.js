d0e120d6e1529db225964cc09c4348062533fe56
EduRiu <94857615+EduRiu@users.noreply.github.com>
2021-12-14 18:41:51 -0300

    Add files via upload

---
+++ TP3/1/alertados.js
@@ -0,0 +1,16 @@
+function alertados() 
+{
+
+    var x = document.getElementById("numero").value
+    
+    if (x<=18) 
+    {
+    alert ("no puedes conducir")
+    }
+    else
+    {
+    alert ("puedes conducir")
+    }
+
+}
+
