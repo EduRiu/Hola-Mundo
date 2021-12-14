b9f129feee0fefc2c3d6948dcfcad1f9e3fa3468
EduRiu <94857615+EduRiu@users.noreply.github.com>
2021-12-14 19:16:37 -0300

    Add files via upload

---
+++ TP3/6-jQ/paroimpar-jQ.js
@@ -0,0 +1,27 @@
+function paroimpar()
+{
+var x = document.getElementById("ingreso").value 
+
+var i = x%2
+
+if (i==0) 
+
+    {   
+    alert ("el numero es par")
+    }
+
+ else 
+
+    {
+    alert ("el numero es impar")
+    }
+}
+
+
+
+function mostrar ()
+{
+
+   $("#oculto").show();
+
+}
